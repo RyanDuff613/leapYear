2e3148b3d59f7bdc2ae172c775d560a1c9e03362
Ryan Duff <ryanduff613@gmail.com>
2020-04-01 09:38:25 -0700

    add initial html form, scripts.js to bring in user data, css to hide result div

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
+// business logic
+var leapYear = function(year){
 
+}
+
+$(document).ready(function() {
+  $('form#leap-year').submit(function(event){
+    event.preventDefault();
+    var year = parseInt($('input#year').val());
+    var result = leapYear(year);
+    $('#result').text(result);
+  });
 });
